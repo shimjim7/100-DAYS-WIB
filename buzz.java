3ea4c5fcde158f14ff8ebb78e9beb668b29f540f
shimjim7 <kamya030btit21@igdtuw.ac.in>
2023-05-20 00:32:36 +0530

    Add files via upload

---
+++ buzz.java
@@ -0,0 +1,11 @@
+public class buzz {
+    public static void main(String args[]){
+        for (let i=1;i<=100;i++){
+            let f = i % 3 ==0,
+           b=i % 5 ==0;
+           console.log(f ? (b ?'fizzbuzz':'fizz') : b ?'buzz':i);
+       }
+    }    
+
+    
+}
